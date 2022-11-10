5d4410e98614fb40ebdb15d4637de9265c4d3676
Jose Vega <josevega234@gmail.com>
2022-11-10 22:49:28 +0100

    replace typos

--- include/np_map.h
+++ include/np_map.h
@@ -7,28 +7,28 @@
 //////////////////////////////////////
 // NEOPIXELS LEFT
 
-byte const NP_PADL_1 = 0;
-byte const NP_PADL_2 = 1;
-byte const NP_PADL_3 = 2;
-byte const NP_PADL_4 = 3;
-byte const NP_PADL_5 = 4;
-byte const NP_PADL_6 = 5;
-byte const NP_PADL_7 = 6;
-byte const NP_PADL_8 = 7;
-byte const NP_RANGE_L = 8;
-byte const NP_SYNC_L = 9;
+uint8_t const NP_PADL_1 = 0;
+uint8_t const NP_PADL_2 = 1;
+uint8_t const NP_PADL_3 = 2;
+uint8_t const NP_PADL_4 = 3;
+uint8_t const NP_PADL_5 = 4;
+uint8_t const NP_PADL_6 = 5;
+uint8_t const NP_PADL_7 = 6;
+uint8_t const NP_PADL_8 = 7;
+uint8_t const NP_RANGE_L = 8;
+uint8_t const NP_SYNC_L = 9;
 
 //////////////////////////////////////
 // NEOPIXELS RIGHT
 
-byte const NP_PADR_8 = 10;
-byte const NP_PADR_7 = 11;
-byte const NP_PADR_6 = 12;
-byte const NP_PADR_5 = 13;
-byte const NP_PADR_4 = 14;
-byte const NP_PADR_3 = 15;
-byte const NP_PADR_2 = 16;
-byte const NP_PADR_1 = 17;
-byte const NP_DECK_SEL = 18;
-byte const NP_RANGE_R = 19;
-byte const NP_SYNC_R = 20;
+uint8_t const NP_PADR_8 = 10;
+uint8_t const NP_PADR_7 = 11;
+uint8_t const NP_PADR_6 = 12;
+uint8_t const NP_PADR_5 = 13;
+uint8_t const NP_PADR_4 = 14;
+uint8_t const NP_PADR_3 = 15;
+uint8_t const NP_PADR_2 = 16;
+uint8_t const NP_PADR_1 = 17;
+uint8_t const NP_DECK_SEL = 18;
+uint8_t const NP_RANGE_R = 19;
+uint8_t const NP_SYNC_R = 20;
