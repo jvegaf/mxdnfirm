69b238b4006046a37f622ee9080d4169abf13785
Jose Vega <josevega234@gmail.com>
2022-12-18 07:43:23 +0100

    pads

--- include/pad_modes.h
+++ include/pad_modes.h
@@ -7,13 +7,21 @@
 // HOTCUE
 // LOOP
 // RANGE
-enum class PadMode : uint8_t {
-HotCue1 = 24,
-HotCue2 = 25,
-HotCue3 = 26,
-HotCue4 = 27,
-HotCue5 = 28,
-HotCue6 = 29,
-HotCue7 = 30,
-HotCue8 = 31,
-};
+
+uint8_t const HotCueMode1 = 24;
+uint8_t const HotCueMode2 = 25;
+uint8_t const HotCueMode3 = 26;
+uint8_t const HotCueMode4 = 27;
+uint8_t const HotCueMode5 = 28;
+uint8_t const HotCueMode6 = 29;
+uint8_t const HotCueMode7 = 30;
+uint8_t const HotCueMode8 = 31;
+
+uint8_t const RangeMode1 = 48;
+uint8_t const RangeMode2 = 49;
+uint8_t const RangeMode3 = 50;
+uint8_t const RangeMode4 = 51;
+uint8_t const RangeMode5 = 52;
+uint8_t const RangeMode6 = 53;
+uint8_t const RangeMode7 = 54;
+uint8_t const RangeMode8 = 55;
